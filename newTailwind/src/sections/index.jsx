503dd448a3ba6b32e694b1d0606e8c758a62401e
Shivendra Tripathi <shivendraking19@gmail.com>
2024-01-25 19:07:26 +0530

    second commit

---
+++ newTailwind/src/sections/index.jsx
@@ -0,0 +1,18 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import Footer from './footer';
+import PopularProduct from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero,
+    PopularProduct,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,Subscribe,
+    Footer
+}
